2356c47d9792b16eb5916dffb28b61f49f0555bd
Nazmi <github@nazmi.fr>
2020-02-01 03:20:06 +0100

    V0.2 - comments, file name, variables, item folder
    added comments, variables and automatic item name scraping from the URL so that the script can be run multiple times to get different items each in their folder. Also the files are now like: PP0001.jpg

--- iadownload.sh
+++ iadownload.sh
@@ -1,27 +1,49 @@
 #!/bin/bash
-ura="https://ia801504.us.archive.org/BookReader/BookReaderImages.php?zip=/32/items/baiestoxiques/Les%20baies%20toxiques_300ppi_jp2.zip&file=Les%20baies%20toxiques_300ppi_jp2/Les%20baies%20toxiques_300ppi_"
-urb=".jp2&scale=4&rotate=0"
-zer="000"
-n="0"
 
-while [ $n -le 12 ]
+# first and second part of url
+# example: https://ia801305.us.archive.org/BookReader/BookReaderImages.php?zip=/6/items/KurtSaxonThePoorMansJamesBondVol5.pdf/Kurt%20Saxon%20-%20The%20Poor%20Mans%20James%20Bond%20-%20Vol%201_jp2.zip&file=Kurt%20Saxon%20-%20The%20Poor%20Mans%20James%20Bond%20-%20Vol%201_jp2/Kurt%20Saxon%20-%20The%20Poor%20Mans%20James%20Bond%20-%20Vol%201_0010.jp2&scale=1&rotate=0
+# please remove the 0003 or so page number and leave all the other characters, you can use any page, just remove the page number
+ura="https://ia801305.us.archive.org/BookReader/BookReaderImages.php?zip=/6/items/KurtSaxonThePoorMansJamesBondVol5.pdf/Kurt%20Saxon%20-%20The%20Poor%20Mans%20James%20Bond%20-%20Vol%201_jp2.zip&file=Kurt%20Saxon%20-%20The%20Poor%20Mans%20James%20Bond%20-%20Vol%201_jp2/Kurt%20Saxon%20-%20The%20Poor%20Mans%20James%20Bond%20-%20Vol%201_"
+urb=".jp2&scale=1&rotate=0"
+
+# The pages will be downloaded from and to those two variables, change accordingly as to the number of pages your document has and the pages you already have downloaded if it applies, archive.org items start with page 0
+from="0"
+to="3"
+
+# change the user agent if needed, the cookies is on the wget line with cookies.txt by default
+useragent="Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)"
+# change extension if not jpeg
+ext=".jpg"
+
+# this is the default number of zeroes for 1 digit numbers, 0000 0001 0002 0003 0004
+zer="000"
+
+# this may cause issues in case the archive.org item name is weird, you may then remove this regex parsing pipe chain and set an arbitrary name for the folder where the images are to be downloaded
+folder="$(echo $ura | sed 's/.*items\/\(.*\)/\1/' | awk -F'/' '{print $1}')"
+mkdir $folder
+
+
+while [ $from -le $to ]
 do
-	if [ $n -ge 10 ]
+# this part of the script adds the required number of arbitrary zeroes to the url, it works up to 9999 pages which should suffice, raise an issue if you find an item with more pages
+	if [ $from -ge 10 ]
 	then
 		zer="00"
 	fi
-	if [ $n -ge 100 ]
+	if [ $from -ge 100 ]
 	then
 		zer="0"
 	fi
-	if [ $n -ge 1000 ]
+	if [ $from -ge 1000 ]
 	then
 		zer=""
 	fi
-  url="$ura$zer$n$urb"
+# these two variables assemble the full url and local path of the downloaded image
+  url="$ura$zer$from$urb"
   echo $url
-  echo "\n\r\n\r"
-  echo $zer$n
-  wget -U "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)" --load-cookies cookies.txt $url
-  n=$[$n+1]
+  name="$folder/PP$zer$from$ext"
+# wget sends the GET HTTP request and saves the file, if the file was already downloaded it'll be overwritten
+  wget -U $useragent --load-cookies cookies.txt $url -O $name
+  let "from++"
 done
+h
