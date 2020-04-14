9a14326c24bc356da15ab54470fbed4257541f95
Nazmi <github@nazmi.fr>
2020-04-14 14:03:06 +0200

    Create iascraper.py

---
+++ iascraper.py
@@ -0,0 +1,5 @@
+# Python implementation of the archive.org booklet scraper for Bash (on GNU/Linux)
+# Fully cross-platform
+# Less giberish in the terminal window (only current page being downloaded and total number of pages to download, no HTTP headers and shiet
+
+TODO
